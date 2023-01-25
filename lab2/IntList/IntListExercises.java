c4998b08eb97a9cd90429462466ab052fa6014ac
Ziqi He <52439236+iheziqi@users.noreply.github.com>
2023-01-25 23:53:12 +0100

    store progress of lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,9 +10,10 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
+        head.first += c;
         while (head.rest != null) {
-            head.first += c;
             head = head.rest;
+            head.first += c;
         }
     }
 
@@ -26,7 +27,8 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            if (firstDigitEqualsLastDigit(currentMax)) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,10 +53,11 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        int firstDigit = 0;
+        while (x !=0 ) {
+            firstDigit = x % 10;
             x = x / 10;
         }
-        int firstDigit = x % 10;
         return firstDigit == lastDigit;
     }
 
